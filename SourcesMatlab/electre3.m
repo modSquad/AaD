ab13fd9fa57150132bac3fb40b8030821493dd8e
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2010-10-12 10:13:52 +0200

    Finalisation de electre3.m, ajout de graph.dot

--- SourcesMatlab/electre3.m
+++ SourcesMatlab/electre3.m
@@ -1,10 +1,12 @@
-function [C,D,S] = electre3 (J,w,s,v)
+function [S,C,D] = electre3 (J,w,s,v)
 % J = m x n judgment matrix
 % w = n weight
 
 n = size(J,1); % alternatives
 m = size(J,2); % indicators
-e = 10 % scale
+e = 10; % scale
+
+graphname = 'graph.dot';
 
 if (m ~= size(w))
     error('Size doesn`t match.');
@@ -12,6 +14,11 @@ end;
     
 C = zeros(n);
 D = zeros(n);
+
+% APPLY NEW SCALE TO JUDGMENT MATRIX
+for i = 1:m
+    J(:,i) = ( (J(:,i) - (e/2)) * (w(i)/max(w))) + (e/2);
+end;
 
 % EVALUATE AGREEMENT-DESAGREEMENT MATRICES
 for i = 1:n
@@ -26,6 +33,20 @@ end;
 % EVALUATE OUTCLASS MATRIX
 S = ((C > s).*(D <= v));
 
+% DRAW ORDER GRAPH
+fid = fopen(graphname, 'w');
+fprintf(fid, 'digraph G {\n');
+[row, col] = find(S);
+k = size(row,1);
+for i = 1:k
+    fprintf(fid, [char(64+row(i)), ' -> ', char(64+col(i)), ';\n']);
+end;
+fprintf(fid, '\n');
+for i = 1:n
+    fprintf(fid, [char(64+i), ';\n']);
+end;
+fprintf(fid, '}');
+fclose(fid);
 % ASCENDING ORDER
 % TODO
 
