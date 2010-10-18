8cbba9e4b1e872b274f25f3cfb64713756255d55
Paul ADENOT <3voker@gmail.com>
2010-10-19 00:36:43 +0200

    Final pour la partie 3

--- SourcesMatlab/OptimisationPartie2.m
+++ SourcesMatlab/OptimisationPartie2.m
@@ -14,17 +14,17 @@ function [ optimized ] = OptimisationPartie2( inputs )
 
     stock = MatPremMax - (inputs' * MatPremParProduit');
     
-    % Pour chaque itération, on enlève un produit de la famille 1, et 
-    % on rajoute un produit de type E si on a les matières premières pour.
-    % À chaque fois, on recalcule les métriques.
+    % Pour chaque iteration, on enleve un produit de la famille 1, et 
+    % on rajoute un produit de type E si on a les matieres premieres pour.
+    % A chaque fois, on recalcule les metriques.
     for a=1:1:100
         if stock(1,1) > MatPremParProduit(1,3)
             if stock(1,2) > MatPremParProduit(2,3)
                 if stock(1,3) > MatPremParProduit(3,3)
-                    % On ajoute un élément au stock : il n'est plus
-                    % fabriqué
+                    % On ajoute un element au stock : il n'est plus
+                    % fabrique
                     stock = stock + MatPremParProduit(1:3,3)';
-                    % On le retire en quantité
+                    % On le retire en quantite
                     inputs(3,1) = inputs(3,1) - 1;
                 end
             end
@@ -35,12 +35,12 @@ function [ optimized ] = OptimisationPartie2( inputs )
                     % On fabrique un nouvel element : on me retire
                     % du stock
                     stock = stock - MatPremParProduit(1:3,5)';
-                    % On l'ajoute en quantité
+                    % On l'ajoute en quantite
                     inputs(5,1) = inputs(5,1) + 1;
                 end
             end
          end
-         % On recalcule les métriques
+         % On recalcule les metriques
         result(a,1:4) = compute_satisfaction(inputs);
         %inputs'
         %stock'
